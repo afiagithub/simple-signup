1647b80b7a12e11d2d30787260e30d958eab9dc3
Afia Rahman <rahmanmou26@gmail.com>
2024-10-11 18:08:11 +0600

    set up project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,13 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        man: ['"Manrope"', "sans-serif"],
+        nun: ['"Nunito"', "sans-serif"],
+      },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require('daisyui'),],
 };
 export default config;
